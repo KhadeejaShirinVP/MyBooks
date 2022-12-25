218c8a246c781a15b8e300ecbed9aa7ca2fc058b
KhadeejaShirinVP <khadeejasvp@moback.com>
2022-12-25 12:51:53 +0530

    skeleton

---
+++ MyBooks/BooksWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BooksWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BooksWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        //Constructor
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options) :base(options)
+        {
+        }
+
+        public DbSet<BookModel> Books { get; set; }
+    }
+}
---
+++ MyBooks/BooksWeb/Models/BookModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksWeb.Models
+{
+    public class BookModel
+    {
+        [Key]
+        public int Book_ID { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Should be greater than or equal to 1")]
+        public int price { get; set; }
+    }
+}
